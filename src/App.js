ed23b392a80361cd701a49db7e14fe836b22d745
Your Name <you@example.com>
2024-03-30 11:07:39 +0100

    Fetch data with premade method

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import { useEffect } from 'react';
-import { useAxiosFetch } from './hooks/useAxiosFetch';
 import { useStoreActions } from 'easy-peasy';
 import Header from './components/Header';
 import TagList from './components/TagList';
 import NavBar from './components/NavBar';
+import PaginationComponent from './components/PaginationComponent';
 
 function App() {
-  const setTags = useStoreActions(actions => actions.setTags);
-  const setMaxPage = useStoreActions(actions => actions.setMaxPage);
-
-  const { data, fetchError } = useAxiosFetch(
-    'https://api.stackexchange.com/2.3/tags?&order=desc&sort=popular&site=stackoverflow'
-  );
-  const { data: totalData, fetchError: totalFetchError } = useAxiosFetch(
-    'https://api.stackexchange.com/2.3/tags?&order=desc&sort=popular&site=stackoverflow&filter=total'
-  );
-  const defaultPageSize = 30;
-  const maxPage = Math.ceil(totalData.total / defaultPageSize);
+  const getTags = useStoreActions(actions => actions.getTags);
 
   useEffect(() => {
-    if (!fetchError) {
-      setTags(data.items);
-    }
-
-    if (!totalFetchError) {
-      setMaxPage(maxPage);
-    }
-  }, [data, setTags, totalData, setMaxPage]);
+    getTags();
+  }, []);
 
   return (
     <div className="App">
       <Header />
       <NavBar />
       <TagList />
+      <PaginationComponent />
     </div>
   );
 }

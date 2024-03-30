d9ce5a9e272ce3abf99e956121c0ee789ea0b7d1
Your Name <you@example.com>
2024-03-30 11:09:40 +0100

    Change layout, remove comment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,16 @@ import Grid from '@mui/material/Unstable_Grid2/Grid2';
 const NavBar = () => {
   return (
     <Grid container spacing={1}>
-      <Grid item xs={12} sm={9} md={3} display="flex" alignItems="center">
+      <Grid xs={12} sm={9} md={3} display="flex" alignItems="center">
         <Search />
       </Grid>
-      <Grid item xs={6} sm={3} md={3} display="flex" alignItems="center">
+      <Grid xs={12} sm={3} md={2} display="flex" alignItems="center">
         <ElementsNumber />
       </Grid>
-      <Grid item xs={6} sm={5} md={3} display="flex" alignItems="center">
+      <Grid xs={12} sm={5} md={3} display="flex" alignItems="center">
         <OrderBy />
       </Grid>
-      <Grid item xs={12} sm={7} md={3} display="flex" alignItems="center">
+      <Grid xs={12} sm={7} md={4} display="flex" alignItems="center">
         <SortBy />
       </Grid>
     </Grid>
@@ -25,15 +25,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-/*
-
-
-    <>
-      <Search />
-      <ElementsNumber />
-      <OrderBy />
-      <SortBy />
-    </>
-
-*/

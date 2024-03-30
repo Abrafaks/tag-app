d45c677ef8994a352c9469fefbd88fde2bb5e514
Your Name <you@example.com>
2024-03-30 11:13:33 +0100

    Handle error and loading states

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -3,21 +3,41 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { useStoreState } from 'easy-peasy';
 import TagItem from './TagItem';
 import { v4 } from 'uuid';
-import { Typography } from '@mui/material';
+import { Stack, CircularProgress, Typography } from '@mui/material';
 
 const TagList = () => {
+  const isLoading = useStoreState(state => state.isLoading);
+  const isError = useStoreState(state => state.isError);
   const tags = useStoreState(state => state.tags);
 
   return (
-    <Grid container spacing={2}>
-      {tags && tags.length > 0 ? (
-        tags.map(({ name, count }) => <TagItem key={v4()} name={name} count={count} />)
-      ) : (
+    <>
+      {isLoading && (
+        <Stack alignItems={'center'} mt={5}>
+          <CircularProgress />
+        </Stack>
+      )}
+
+      {isError && (
         <Typography variant="body1" gutterBottom>
+          Error occurred
+        </Typography>
+      )}
+
+      {!isError && !isLoading && tags && tags.length > 0 && (
+        <Grid container spacing={2} mt={5} height={'auto'}>
+          {tags.map(({ name, count }) => (
+            <TagItem key={v4()} name={name} count={count} />
+          ))}
+        </Grid>
+      )}
+
+      {!isError && !isLoading && tags && tags.length === 0 && (
+        <Typography variant="body1" gutterBottom mt={5}>
           No tags to display
         </Typography>
       )}
-    </Grid>
+    </>
   );
 };
 
